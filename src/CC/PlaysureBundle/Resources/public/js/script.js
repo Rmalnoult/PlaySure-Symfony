667a0a8fcbfa3caeffeb07b2c4c19bedfbf03cfd
Rmalnoult <romainmalnoult@gmail.com>
2014-12-12 05:06:06 +0100

    card css

--- src/CC/PlaysureBundle/Resources/public/js/script.js
+++ src/CC/PlaysureBundle/Resources/public/js/script.js
@@ -1,13 +1,84 @@
  $( document ).ready(function() {
 	 	console.log('hello world progresss');
-	 	var ActionsSold = $('span.numberOfActionsSold').text();
-	 	var ActionsTotal = $('span.numberOfActionsTotal').text();
+	 	var ActionsSold1 = $('span.numberOfActionsSold1').text();
+	 	var ActionsTotal1 = $('span.numberOfActionsTotal1').text();
 	 	console.log();
 
-	    $('#sample_goal').goalProgress({
-	        goalAmount: ActionsTotal,
-	        currentAmount: ActionsSold,
+	    $('#bar1').goalProgress({
+	        goalAmount: ActionsTotal1,
+	        currentAmount: ActionsSold1,
 	        textBefore: '',
 	        textAfter: ' '
 	    });
+	    	var ActionsSold2 = $('span.numberOfActionsSold2').text();
+	    	var ActionsTotal2 = $('span.numberOfActionsTotal2').text();
+	    	console.log();
+
+	       $('#bar2').goalProgress({
+	           goalAmount: ActionsTotal2,
+	           currentAmount: ActionsSold2,
+	           textBefore: '',
+	           textAfter: ' '
+	       });
+	       	var ActionsSold3 = $('span.numberOfActionsSold3').text();
+	       	var ActionsTotal3 = $('span.numberOfActionsTotal3').text();
+	       	console.log();
+
+	          $('#bar3').goalProgress({
+	              goalAmount: ActionsTotal3,
+	              currentAmount: ActionsSold3,
+	              textBefore: '',
+	              textAfter: ' '
+	          });
+	          	var ActionsSold4 = $('span.numberOfActionsSold4').text();
+	          	var ActionsTotal4 = $('span.numberOfActionsTotal4').text();
+	          	console.log();
+
+	             $('#bar4').goalProgress({
+	                 goalAmount: ActionsTotal4,
+	                 currentAmount: ActionsSold4,
+	                 textBefore: '',
+	                 textAfter: ' '
+	             });
+	             	var ActionsSold5 = $('span.numberOfActionsSold5').text();
+	             	var ActionsTotal5 = $('span.numberOfActionsTotal5').text();
+	             	console.log();
+
+	                $('#bar5').goalProgress({
+	                    goalAmount: ActionsTotal5,
+	                    currentAmount: ActionsSold5,
+	                    textBefore: '',
+	                    textAfter: ' '
+	                });
+	                	var ActionsSold6 = $('span.numberOfActionsSold6').text();
+	                	var ActionsTotal6 = $('span.numberOfActionsTotal6').text();
+	                	console.log();
+
+	                   $('#bar6').goalProgress({
+	                       goalAmount: ActionsTotal6,
+	                       currentAmount: ActionsSold6,
+	                       textBefore: '',
+	                       textAfter: ' '
+	                   });
+	                   	var ActionsSold7 = $('span.numberOfActionsSold7').text();
+	                   	var ActionsTotal7 = $('span.numberOfActionsTotal7').text();
+	                   	console.log();
+
+	                      $('#bar7').goalProgress({
+	                          goalAmount: ActionsTotal7,
+	                          currentAmount: ActionsSold7,
+	                          textBefore: '',
+	                          textAfter: ' '
+	                      });
+	                      	var ActionsSold8 = $('span.numberOfActionsSold8').text();
+	                      	var ActionsTotal8 = $('span.numberOfActionsTotal8').text();
+	                      	console.log();
+
+	                         $('#bar8').goalProgress({
+	                             goalAmount: ActionsTotal8,
+	                             currentAmount: ActionsSold8,
+	                             textBefore: '',
+	                             textAfter: ' '
+	                         });
+
 });
